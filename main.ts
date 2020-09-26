dc3c8ea52fbe38937aad6c6e2b2b86036d60aa0d
soniamgarzon <32686551+soniamgarzon@users.noreply.github.com>
2020-09-26 08:25:18 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+basic.forever(function () {
+    basic.showNumber(input.temperature())
+    if (input.temperature() > 25) {
+        basic.showNumber(input.temperature())
+        basic.showIcon(IconNames.Happy)
+        basic.pause(500)
+    } else {
+        basic.showNumber(input.temperature())
+        basic.showIcon(IconNames.Sad)
+        basic.pause(500)
+    }
+})
